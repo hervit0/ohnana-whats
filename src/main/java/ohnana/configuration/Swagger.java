a9a6b1b625c5db7545abea54c720fb70f96cc7d1
Hervé Ah-Leung <herve@theappbusiness.com>
2017-11-14 23:03:13 +0000

    Add Swagger

---
+++ src/main/java/ohnana/configuration/Swagger.java
@@ -0,0 +1,33 @@
+package ohnana.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableSwagger2
+public class Swagger {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, Arrays.asList(
+                        new ResponseMessageBuilder().code(400).message("Client input").build(),
+                        new ResponseMessageBuilder().code(500).message("Server error").build())
+                );
+    }
+
+}
--- src/main/java/ohnana/controller/InfoController.java
+++ src/main/java/ohnana/controller/InfoController.java
@@ -1,7 +1,8 @@
 package ohnana.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -9,10 +10,15 @@ import java.time.ZonedDateTime;
 @RestController
 public class InfoController {
 
-    @RequestMapping("/")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public String index() {
         String time = ZonedDateTime.now(ZoneId.of("Europe/London")).toString();
         return String.format("Welcome Ohnana - %s", time);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    public String test() {
+        return "Back to the future - are we?";
     }
 
 }
