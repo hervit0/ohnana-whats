6a14f14bba9d757b30671864ab480c20cb721f1f
Hervé Ah-Leung <herve.ahleung@gmail.com>
2018-02-04 13:55:49 +0000

    NA-41: Plug Java seeder to Elixir scraper

---
+++ src/main/java/ohnana/persistence/seeder/Parser.java
@@ -0,0 +1,46 @@
+package ohnana.persistence.seeder;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import ohnana.model.Card;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class Parser {
+    public static final String RESOURCE = "static/cards.json";
+
+    public List<Card> getCards() {
+        JSONParser parser = new JSONParser();
+        List<Card> cardList = new ArrayList<>();
+
+        try {
+            String json = Resources.toString(Resources.getResource(RESOURCE), Charsets.UTF_8);
+            JSONArray cardsList = (JSONArray) parser.parse(json);
+
+            cardList = (List<Card>) cardsList.parallelStream()
+                    .map(Parser::parseCard)
+                    .collect(Collectors.toList());
+        } catch (IOException | ParseException exception) {
+            exception.printStackTrace();
+        }
+
+        return cardList;
+    }
+
+    private static Object parseCard(Object cardObject) {
+        JSONObject card = (JSONObject) cardObject;
+
+        return Card.builder()
+                .id(UUID.randomUUID())
+                .name((String) card.get("name"))
+                .build();
+    }
+}
--- src/main/java/ohnana/persistence/seeder/Seeder.java
+++ src/main/java/ohnana/persistence/seeder/Seeder.java
@@ -1,21 +1,20 @@
 package ohnana.persistence.seeder;
 
-import ohnana.model.Card;
 import ohnana.persistence.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class Seeder {
     private CardRepository cardRepository;
+    private Parser parser;
 
     @Autowired
     public Seeder(CardRepository cardRepository) {
         this.cardRepository = cardRepository;
+        this.parser = new Parser();
     }
 
     @EventListener
@@ -24,10 +23,6 @@ public class Seeder {
     }
 
     private void seedCardsTable() {
-        Card card = Card.builder()
-                .id(UUID .randomUUID())
-                .name("Seeded from Java")
-                .build();
-        cardRepository.save(card);
+        parser.getCards().forEach(card -> cardRepository.save(card));
     }
 }
